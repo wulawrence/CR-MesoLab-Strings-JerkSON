9d2c5e629b14520c02704b65cc3ff4d885da4947
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-03-16 18:41:55 -0400

    pattern+matcher for all the items

--- src/main/java/io/zipcoder/ItemParseException.java
+++ src/main/java/io/zipcoder/ItemParseException.java
@@ -1,4 +1,5 @@
 package io.zipcoder;
 
 public class ItemParseException extends Exception {
+    //what is this for?
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -2,9 +2,13 @@ package io.zipcoder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemParser {
 
+    List<String> easyRead;
 
     public ArrayList<String> parseRawDataIntoStringArray(String rawData){
         String stringPattern = "##";
@@ -13,11 +17,12 @@ public class ItemParser {
     }
 
     public Item parseStringIntoItem(String rawItem) throws ItemParseException{
+
         return null;
     }
 
     public ArrayList<String> findKeyValuePairsInRawItemData(String rawItem){
-        String stringPattern = "[;|^]";
+        String stringPattern = "[;|^|%|@|*|!]";
         ArrayList<String> response = splitStringWithRegexPattern(stringPattern , rawItem);
         return response;
     }
@@ -26,6 +31,36 @@ public class ItemParser {
         return new ArrayList<String>(Arrays.asList(inputString.split(stringPattern)));
     }
 
+    private boolean findName(String rawItem){
+        String regexName = "(N|n)(A|a)(M|m)(E|e):\\w+";
+        Pattern name = Pattern.compile(regexName);
+        Matcher match = name.matcher(rawItem);
+        return match.find();
+    }
+    private boolean findPrice(String rawItem){
+        String regexPrice = "(P|p)(R|r)(I|i)(C|c)(E|e):\\d+";
+        Pattern price = Pattern.compile(regexPrice);
+        Matcher match = price.matcher(rawItem);
+        return match.find();
+    }
+    private boolean findType(String rawItem){
+        String regexType = "(T|t)(Y|y)(P|p)(E|e):\\w+";
+        Pattern type = Pattern.compile(regexType);
+        Matcher match = type.matcher(rawItem);
+        return match.find();
+    }
+    private boolean findExpiration(String rawItem){
+        String regexExp = "(E|e)........(N|n):\\d.......\\d";
+        Pattern expire = Pattern.compile(regexExp);
+        Matcher match = expire.matcher(rawItem);
+        return match.find();
 
+    }
+
+    public static void main(String[] args) {
+        ItemParser test = new ItemParser();
+        ArrayList testy = test.parseRawDataIntoStringArray("naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food^expiration:1/04/2016##");
+        System.out.println(testy.size());
+    }
 
 }
